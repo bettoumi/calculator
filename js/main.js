21568eb80649aaa7b705df114f50f57a564522dc
sahbia <ouaja.sahbia@gmail.com>
2017-07-12 12:03:20 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+        // Adition
+        //----------
+function adition(a,b)
+{
+  return a+b;
+}
+        //  multiplication
+        //  -------------- 
+function multiplication(a,b)
+{
+  return a*b;
+}
+
+   // division
+   // ---------
+function division(a,b)
+{
+  if(b!==0)
+    {
+      return a/b;
+    }
+  else{ alert("division par zéro impossible");}
+}
+  //soustraction 
+  //------------
+function soustraction (a,b)
+{
+  return  a-b;
+}
+   // pourcentage
+   // -----------
+function pourcentage(a)
+{
+  return a/100;
+}
+     // racine carré
+     // ------------
+function racinecarre(a)
+{
+  return Math.sqrt(a);
+}
+    //signe
+    //-----
+function signe(a)
+{
+  if(a>0){ return -a;}
+  else {return --a;}
+}
+
+
+function echape()
+{
+
+}
